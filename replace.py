146ca331c06267cd39e304912b94a744f40e2b0e
sdworczyk <129424222+sdworczyk@users.noreply.github.com>
2023-03-30 18:43:37 +0100

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,7 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog!."
+replace = print(sentence.replace("!"," "))
+upper = print(sentence.replace("!"," ").upper())
+spli = print(sentence.replace("!"," ").upper().split())
+new = (sentence.replace("!"," ").upper().split())
+reversed = print(new[::-1])
+reversed = print(" ".join(new[::-1]))
