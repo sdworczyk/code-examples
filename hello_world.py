b01f9375f1f21625f5c06e80d6226c025ddb9a22
sdworczyk <129424222+sdworczyk@users.noreply.github.com>
2023-03-30 18:42:08 +0100

    Add files via upload
    The very first tasks for the boot camp.

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name: ")
+user_age = input("Please state your age: ")
+print("Hello " + user_name + "! You are " + user_age + " years old!")
+
+print("Hello World")
+
+
---
+++ menu.py
@@ -0,0 +1,6 @@
+print("Please order 3 items at a time")
+item1 = input("Please state the first item: ")
+item2 = input("Please state the second item: ")
+item3 = input("Please state the third item: ")
+print("Order confirmed! You have ordered " + item1 + " " + item2 + " and " + item3 +
+      ". The order will be with you shortly.")
