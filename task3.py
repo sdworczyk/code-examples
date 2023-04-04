8687017dd01ade4105cf7fe7787f8db4574a9c32
sdworczyk <129424222+sdworczyk@users.noreply.github.com>
2023-04-04 23:44:09 +0100

    Delete task3.py

--- task3.py
+++
@@ -1,8 +0,0 @@
-
-row = 5
-# outer loop defining how many rows I need in the loop
-for x in range(1, row + 1):
-    # inner loop determines that in each loop there will be one additional print in the column
-    for y in range(1, x + 1):
-        print("*", end="")
-    print()
