1affa280f3113c4b12f031dc8f26b94e557cd3d5
sdworczyk <129424222+sdworczyk@users.noreply.github.com>
2023-04-04 23:43:37 +0100

    Add files via upload
    'For' loop usage to make a cascading asterisk table.

--- task3.py
+++ task3.py
@@ -1,18 +1,8 @@
-participant = input("Please enter your name: ")
-swimming = int(input("Please state the time of completion in minutes for the swimming event: "))
-cycling = int(input("Please state the time of completion in minutes for the cycling event: "))
-running = int(input("Please state the time of completion in minutes for the running event: "))
-total = swimming + cycling + running
-print("Congratulations on completing the triathlon", participant, "!", "Your total time is: ", total, " minutes.")
-#Each discipline received its own input with float command. This helps with the addition of all times for the total string.
-if (total <= 100): #More than or equal to sign makes sure that the right award is printed for the time provided.
-    print("Well done! You receive a Provincial Colours award!")
 
-elif (total > 100) and (total <= 105): #The range od more than and less than and equal signs makes sure to print appropriate message for time provided.
-    print("Well done! You receive a Provincial Half Colours award!")
-
-elif (total > 105) and (total <= 110): #The range od more than and less than and equal signs makes sure to print appropriate message for time provided.
-    print("Well done! You receive a Provincial Scroll award!")
-
-elif (total > 110): #A simple use of more than sign allows to print appropriate message.
-    print("Well done! You have completed the triathlon. Unfortunately no reward is available for you at this time. Good luck next time!")
+row = 5
+# outer loop defining how many rows I need in the loop
+for x in range(1, row + 1):
+    # inner loop determines that in each loop there will be one additional print in the column
+    for y in range(1, x + 1):
+        print("*", end="")
+    print()
